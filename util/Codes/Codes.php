3f23ab0810f3679f7eb56eda3bca44e2cf4b6aa5
waseem <waseem.khan@claritusconsulting.com>
2019-02-14 14:34:24 +0530

    util folder

---
+++ util/Codes/Codes.php
@@ -0,0 +1,20 @@
+<?php
+    namespace common\web\util\Codes;
+ 
+/*
+ *@AUTHOR: Prachi
+ *@DATE:07-03-2016
+ *  
+ */
+//CONST
+
+    class Codes{
+        
+        CONST SUCCESS =200;
+        CONST ERROR =100;    
+        CONST DB_TECH_ERROR=1001;        
+        CONST VALIDATION_ERROR=1002;
+        CONST EXCEPTION_ERROR=1003;
+       
+    }
+?>
